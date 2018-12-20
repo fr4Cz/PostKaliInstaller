40a5ea4c832897c9e12a7833ea6e966af58c94da
Zorko <MOCHZ@users.noreply.github.com>
2018-12-20 13:09:01 +0100

    Update pk_install.py

--- pk_install.py
+++ pk_install.py
@@ -6,7 +6,7 @@
 #   after the initial installation of the regular 64 bit version of Kali Linux.
 #
 #   For configuring this script according to your own needs please review the packages in the
-#   _apt_packages() and _git_packages() functions and change them according to your preferred configuration.
+#   _apt_packages(), _git_packages(), __wget_packages() methods and change them according to your preferred configuration.
 #   If you are new to Kali Linux then you should be ok running the vanilla configuration.
 #
 #
