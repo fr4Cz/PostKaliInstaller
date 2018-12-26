3e813ebef0df5e0a7ad51d7e42ddab50cc6ca466
Zorko <contact@zorko.co>
2018-12-26 22:31:47 +0100

    Added full path of all used binaries in script

--- packages/config/add_user.sh
+++ packages/config/add_user.sh
@@ -6,5 +6,5 @@
 USR="user"
 PSW="Kali"
 
-useradd -u 20001 -g users -d /home/${USR} -s /bin/bash -p $(echo ${PSW} | openssl passwd -1 -stdin) ${USR}
-echo "INFO: Added new user with username: ${USR} and password: ${PSW}, REMEMBER TO CHANGE THE PASSWORD BEFORE USING THE SYSTEM!" >> ../../pk_install.log
+/usr/sbin/useradd -u 20001 -g users -d /home/${USR} -s /bin/bash -p $(/usr/bin/echo ${PSW} | /usr/bin/openssl passwd -1 -stdin) ${USR}
+/usr/bin/echo "INFO: Added new user with username: ${USR} and password: ${PSW}, REMEMBER TO CHANGE THE PASSWORD BEFORE USING THE SYSTEM!" >> ../../pk_install.log
