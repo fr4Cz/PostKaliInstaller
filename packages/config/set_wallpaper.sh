adf780065f7d7fbda258cd4f9f3b2ce01ee329c2
Zorko <contact@zorko.co>
2019-01-11 00:13:20 +0100

    fixed wallpaper script removed atom script from launch

--- packages/config/set_wallpaper.sh
+++ packages/config/set_wallpaper.sh
@@ -6,7 +6,7 @@ _WP="/home/${PKIUSR}/Pictures/wallpapers"
 _IMG="file://${_WP}/wp.png"
 
 /usr/bin/mkdir -p ${_WP} 2>> ${PKILOG}
-/usr/bin/cp ${PKIPKGROOT}/wp.png ${_WP} 2>> ${PKILOG}
+/usr/bin/cp ${PKIPKGROOT}/packages/wallpapers/wp.png ${_WP} 2>> ${PKILOG}
 /usr/bin/chown -R ${PKIUSR}:staff ${_WP} 2>> ${PKILOG}
 
 /usr/bin/gsettings set org.gnome.desktop.background picture-uri ${_IMG} 2>> ${PKILOG}
