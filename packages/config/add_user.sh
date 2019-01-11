2b637a7a5065a01b57119d5b3b3bee78b5540d48
Zorko <contact@zorko.co>
2019-01-11 11:17:58 +0100

    Fixed user add issue

--- packages/config/add_user.sh
+++ packages/config/add_user.sh
@@ -4,7 +4,7 @@
 #   Remember that it is never a good idea to run as root.
 #
 /usr/bin/mkdir -p /home/${PKIUSR} 2>> ${PKILOG}
-/usr/sbin/useradd -u ${PKIUID} -g staff,sudo,bluetooth,beef,wireshark,postgres,cdrom,audio,kismet -d /home/${PKIUSR} -s /bin/bash -p $(/usr/bin/echo ${PKIPSW} | /usr/bin/openssl passwd -1 -stdin) ${PKIUSR} 2>> ${PKILOG}
+/usr/sbin/useradd -u ${PKIUID} -g staff -G sudo,bluetooth,cdrom,audio -d /home/${PKIUSR} -s /bin/bash -p $(/usr/bin/echo ${PKIPSW} | /usr/bin/openssl passwd -1 -stdin) ${PKIUSR} 2>> ${PKILOG}
 /usr/bin/chown -R -f ${PKIUSR}:staff /home/${PKIUSR} 2>> ${PKILOG}
 /usr/bin/chmod 755 /home/${PKIUSR} 2>> ${PKILOG}
 /usr/bin/echo "INFO: Added new user with username: ${PKIUSR} and password: ${PKIPSW}, REMEMBER TO CHANGE THE PASSWORD BEFORE USING THE SYSTEM!" >> ${PKILOG}
