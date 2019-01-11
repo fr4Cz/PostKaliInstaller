0eff572a5ff78f90109bbe79a0325a3105f854e7
Zorko <contact@zorko.co>
2019-01-11 10:52:34 +0100

    Fixed issues in wallpaper and user scripts

--- packages/config/set_wallpaper.sh
+++ packages/config/set_wallpaper.sh
@@ -5,7 +5,7 @@
 _WP="/home/${PKIUSR}/Pictures"
 _IMG="file://${_WP}/wp.png"
 
-/usr/bin/cp ${PKIPKGROOT}/packages/wallpapers/wp.png ${_WP} 2>> ${PKILOG}
+/usr/bin/cp ${PKIPKGROOT}/packages/wallpapers/wp.png ${_WP}/wp.png 2>> ${PKILOG}
 /usr/bin/chown -R ${PKIUSR}:staff ${_WP} 2>> ${PKILOG}
 
 /usr/bin/gsettings set org.gnome.desktop.background picture-uri ${_IMG} 2>> ${PKILOG}
